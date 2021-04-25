70abee14371ea04ea0039e16158517d5d8ee86b1
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2021-04-25 16:32:42 +0200

    ci: schedule pusher every 3 minutes

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,18 @@ set -e
 
 source ./project.config.sh
 
-CONTAINER_NAME="deoetorbi_notificationpusher"
-CLOUDRUN_TAG="gcr.io/${GCP_PROJECT_ID}/${CONTAINER_NAME}"
+SERVICE_NAME="deoetorbi-notificationpusher"
+CLOUDRUN_TAG="gcr.io/${GCP_PROJECT_ID}/${SERVICE_NAME}"
 
 gcloud config set project "${GCP_PROJECT_ID}"
 
 gcloud builds submit --tag "${CLOUDRUN_TAG}"
 
-gcloud beta run deploy "${CONTAINER_NAME}" \
+gcloud beta run deploy "${SERVICE_NAME}" \
           --region "${GCP_REGION_CLOUDRUN}" \
           --image "${CLOUDRUN_TAG}" \
           --cpu 1 \
           --memory 128Mi \
-          --min-instances 0 \
           --max-instances 1 \
           --timeout 5s \
           --concurrency 1 \
@@ -25,3 +24,15 @@ gcloud beta run deploy "${CONTAINER_NAME}" \
           --port 8080 \
           --platform managed
 
+SERVICE_URL=$(gcloud run services describe ${SERVICE_NAME} --format 'value(status.url)' --platform managed --region=${GCP_REGION_CLOUDRUN})
+SCHEDULER_SERVICE_ACCOUNT_EMAIL="deo-scheduler@deoetorbi.iam.gserviceaccount.com"
+SCHEDULER_JOB="schedule-notifications-pusher-job"
+
+gcloud scheduler jobs delete --quiet "${SCHEDULER_JOB}" || echo "Failed to delete scheduler job"
+gcloud scheduler jobs create http ${SCHEDULER_JOB} --schedule "*/3 * * * *" \
+   --http-method=GET \
+   --uri="${SERVICE_URL}" \
+   --oidc-service-account-email="${SCHEDULER_SERVICE_ACCOUNT_EMAIL}"   \
+   --oidc-token-audience="${SERVICE_URL}"
+
+echo "Done"
